190a5a99035726a5627cf8436154076b7bbe8fd9
Ilyas Ronef <ilyas@divandesign.biz>
2016-11-04 16:26:29 +0500

    * Refactoring, the widget now using named parameters (with backward compatibility).

--- ddselectdocuments/ddselectdocuments.php
+++ ddselectdocuments/ddselectdocuments.php
@@ -8,15 +8,16 @@
  * @uses PHP >= 5.4.
  * @uses MODXEvo.plugin.ManagerManager >= 0.7.
  * 
- * @param $fields {string_commaSeparated} — TVs names that the widget is applied to. @required
- * @param $roles {string_commaSeparated} — Roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
- * @param $templates {string_commaSeparated} — Templates IDs for which the widget is applying (empty value means the widget is applying to all templates). Default: ''.
- * @param $parentIds {string_commaSeparated} — Parent documents IDs. Default: '0'.
- * @param $depth {integer} — Depth of search. Default: 1.
- * @param $filter {separated string} — Filter clauses, separated by '&' between pairs and by '=' between keys and values. For example, 'template=15&published=1' means to choose the published documents with template id=15. Default: ''.
- * @param $maxSelectedItems {integer} — The largest number of elements that can be selected by user (“0” means selection without a limit). Default: 0.
- * @param $listItemLabelMask {string} — Template to be used while rendering elements of the document selection list. It is set as a string containing placeholders for document fields and TVs. Also, there is the additional placeholder “[+title+]” that is substituted with either “menutitle” (if defined) or “pagetitle”. Default: '[+title+] ([+id+])'.
- * @param $allowDuplicates {boolean} — Allows to select duplicates values. Default: false.
+ * @param $params {array_associative|stdClass} — The object of params. @required
+ * @param $params['fields'] {string_commaSeparated} — TVs names that the widget is applied to. @required
+ * @param $params['parentIds'] {string_commaSeparated} — Parent documents IDs. Default: '0'.
+ * @param $params['depth'] {integer} — Depth of search. Default: 1.
+ * @param $params['filter'] {separated string} — Filter clauses, separated by '&' between pairs and by '=' between keys and values. For example, 'template=15&published=1' means to choose the published documents with template id=15. Default: ''.
+ * @param $params['listItemLabelMask'] {string} — Template to be used while rendering elements of the document selection list. It is set as a string containing placeholders for document fields and TVs. Also, there is the additional placeholder “[+title+]” that is substituted with either “menutitle” (if defined) or “pagetitle”. Default: '[+title+] ([+id+])'.
+ * @param $params['maxSelectedItems'] {integer} — The largest number of elements that can be selected by user (“0” means selection without a limit). Default: 0.
+ * @param $params['allowDuplicates'] {boolean} — Allows to select duplicates values. Default: false.
+ * @param $params['roles'] {string_commaSeparated} — Roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
+ * @param $params['templates'] {string_commaSeparated} — Templates IDs for which the widget is applying (empty value means the widget is applying to all templates). Default: ''.
  * 
  * @event OnDocFormPrerender
  * @event OnDocFormRender
@@ -26,18 +27,45 @@
  * @copyright 2013–2016 DivanDesign {@link http://www.DivanDesign.biz }
  */
 
-function mm_ddSelectDocuments(
-	$fields = '',
-	$roles = '',
-	$templates = '',
-	$parentIds = '0',
-	$depth = 1,
-	$filter = '',
-	$maxSelectedItems = 0,
-	$listItemLabelMask = '[+title+] ([+id+])',
-	$allowDuplicates = false
-){
-	if (!useThisRule($roles, $templates)){return;}
+function mm_ddSelectDocuments($params){
+	//For backward compatibility
+	if (
+		//The only one required “fields” parameter
+		is_string($params) ||
+		//Or not
+		func_num_args() > 1
+	){
+		//Convert ordered list of params to named
+		$params = ddTools::orderedParamsToNamed([
+			'paramsList' => func_get_args(),
+			'compliance' => [
+				'fields',
+				'roles',
+				'templates',
+				'parentIds',
+				'depth',
+				'filter',
+				'maxSelectedItems',
+				'listItemLabelMask',
+				'allowDuplicates'
+			]
+		]);
+	}
+	
+	//Defaults
+	$params = (object) array_merge([
+		'fields' => '',
+		'parentIds' => '0',
+		'depth' => 1,
+		'filter' => '',
+		'listItemLabelMask' => '[+title+] ([+id+])',
+		'maxSelectedItems' => 0,
+		'allowDuplicates' => false,
+		'roles' => '',
+		'templates' => ''
+	], (array) $params);
+	
+	if (!useThisRule($params->roles, $params->templates)){return;}
 	
 	global $modx;
 	$e = &$modx->Event;
@@ -57,16 +85,16 @@ function mm_ddSelectDocuments(
 	}else if ($e->name == 'OnDocFormRender'){
 		global $mm_current_page;
 		
-		$fields = tplUseTvs($mm_current_page['template'], $fields);
-		if ($fields == false){return;}
+		$params->fields = tplUseTvs($mm_current_page['template'], $params->fields);
+		if ($params->fields == false){return;}
 		
-		$filter = ddTools::explodeAssoc($filter, '&', '=');
+		$params->filter = ddTools::explodeAssoc($params->filter, '&', '=');
 		
 		//Необходимые поля
-		preg_match_all('~\[\+([^\+\]]*?)\+\]~', $listItemLabelMask, $matchField);
+		preg_match_all('~\[\+([^\+\]]*?)\+\]~', $params->listItemLabelMask, $matchField);
 		
 		$listDocs_fields = array_unique(array_merge(
-			array_keys($filter),
+			array_keys($params->filter),
 			['pagetitle', 'id'],
 			$matchField[1]
 		));
@@ -154,10 +182,10 @@ function mm_ddSelectDocuments(
 		
 		//Получаем все дочерние документы
 		$listDocs = ddGetDocs(
-			explode(',', $parentIds),
-			$filter,
-			$depth,
-			$listItemLabelMask,
+			explode(',', $params->parentIds),
+			$params->filter,
+			$params->depth,
+			$params->listItemLabelMask,
 			$listDocs_fields
 		);
 		
@@ -167,10 +195,10 @@ function mm_ddSelectDocuments(
 		
 		$output .= '//---------- mm_ddSelectDocuments :: Begin -----'.PHP_EOL;
 		
-		foreach ($fields as $field){
+		foreach ($params->fields as $field){
 			$output .=
 '
-$j("#tv'.$field['id'].'").ddMultipleInput({source: '.$listDocs.', max: '.(int) $maxSelectedItems.', allowDoubling: '.(int) $allowDuplicates.'});
+$j("#tv'.$field['id'].'").ddMultipleInput({source: '.$listDocs.', max: '.(int) $params->maxSelectedItems.', allowDoubling: '.(int) $params->allowDuplicates.'});
 ';
 		}
 		
